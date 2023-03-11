c1ab0e5457aecbe9f6336254796316def40e6b98
mimmik <123939490+mimmik@users.noreply.github.com>
2023-03-11 14:48:50 +0200

    Update converters.py
    Changes after pylint testing

--- productsapi/converters.py
+++ productsapi/converters.py
@@ -1,31 +1,33 @@
-from productsapi.db import User, Category
+"""
+Converters are in their own file since they're mapped to app.url_map config
+And it seemed more logical to do the mapping in app.py
+Than cross-import app here
+"""
 from werkzeug.routing import BaseConverter
 from werkzeug.exceptions import NotFound
-
-# Converters are in their own file since they're mapped to app.url_map config
-# And it seemed more logical to do the mapping in app.py
-# Than cross-import app here
+from productsapi.db import User, Category
 
 class UserConverter(BaseConverter):
-
-    def to_python(self, user_name):
-        db_user = User.query.filter_by(name=user_name).first()
+    """
+    This class converts the user from or to url.
+    """
+    def to_python(self, value):
+        db_user = User.query.filter_by(name=value).first()
         if db_user is None:
             raise NotFound
         return db_user
-
     def to_url(self, db_user):
         return db_user.name
 
-
 class CategoryConverter(BaseConverter):
-
-    def to_python(self, cat_name):
-        db_category = Category.query.filter_by(name=cat_name).first()
+    """
+    This class converts the category from or to url.
+    """
+    def to_python(self, value):
+        db_category = Category.query.filter_by(name=value).first()
         if db_category is None:
             raise NotFound
         return db_category
-    
     def to_url(self, db_category):
         return db_category.name
 
